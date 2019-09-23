1463748cb1d426479964fb19ea3e4d960130f00a
iamhrishikesh <33514308+Hrishikesh-Padhye@users.noreply.github.com>
2019-09-23 12:11:17 +0530

    Update and rename mongostart.sh to 2.mongostart.sh

---
+++ 2.mongostart.sh
@@ -0,0 +1,4 @@
+
+cd /home/dbsl/mongodb-linux-x86_64-2.6.3/bin/		#Change the path to the directory where your bin files of MongoDB are located.
+read address		#Enter the address of your MongoDB Server
+./mongo $address/te1148db -u te1148 -p #
