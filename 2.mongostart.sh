ec5a53b2107adef7c9e6fdd6a1c885aa0b49c0cd
iamhrishikesh <33514308+Hrishikesh-Padhye@users.noreply.github.com>
2019-09-24 10:39:36 +0530

    Update 2.mongostart.sh
    Accepts address, database name, username from the user

--- 2.mongostart.sh
+++ 2.mongostart.sh
@@ -1,4 +1,9 @@
 
 cd /home/dbsl/mongodb-linux-x86_64-2.6.3/bin/		#Change the path to the directory where your bin files of MongoDB are located.
+echo "Enter the address of your MongoDB server : "
 read address		#Enter the address of your MongoDB Server
-./mongo $address/te1148db -u te1148 -p #
+echo "Enter your database name : "
+read dbname
+echo "Enter your username : "
+read uname
+./mongo $address/$dbname -u te1148 -p #
