89f0b57466d0f765ce926855b3ce6f52ecda829d
iamhrishikesh <33514308+Hrishikesh-Padhye@users.noreply.github.com>
2019-10-04 23:34:12 +0530

    Add files via upload
    A tool to perform Google search without manually opening your browser.

---
+++ Googleit.sh
@@ -0,0 +1,10 @@
+Google() {
+    search=""
+    echo "Opening Google search for : $@"
+    for term in $@; do
+        search="$search%20$term"
+    done
+    xdg-open "http://www.google.com/search?q=$search"
+}
+
+Google $1
